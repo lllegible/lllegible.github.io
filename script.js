fe0b13f1038d5fefff2fc38df636477e7d862208
Brendan Li <brendanli@Dont-even-try-airdropping-here-2.local>
2023-10-08 19:22:04 -0700

    this is my v1

---
+++ script.js
@@ -0,0 +1,33 @@
+//var let const
+const x = 10;
+x = 12;
+console.log(x);
+
+
+
+/* Hoist
+console.log(x);
+var x = 5; 
+in the above code, even though x is logged befored it declaration. it doesn't an error. This is because the "var x" declaration is hoisted to the top of the code.
+*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// let button = document.getElementById("submitButton");
+// button.addEventListener("click", function () {
+//   alert("Summit succeeded");
+//   alert(10+1+4);
+// });
+
+
